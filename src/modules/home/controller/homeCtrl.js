f66285dcca7859fbad58ff66a7d559df9ea97e39
Rallkus <serhuegi@gmail.com>
2018-11-06 21:09:47 +0100

    Added a new call to tarifas in homeCtrl, now home.html has a section where we are putting the tarifas, modified style.css to add tarifas and added a view in /src when we have tarifa.html due to his high chance to be reusable

--- src/modules/home/controller/homeCtrl.js
+++ src/modules/home/controller/homeCtrl.js
@@ -1,6 +1,7 @@
 import {get} from '../../../utils/utils';
 import {Settings} from '../../../settings';
 import {template} from '../view/home.html';
+import {tarifa} from '../../../view/tarifa.html';
 
 class HomeController {
 
@@ -11,10 +12,28 @@ class HomeController {
     /** render  */
     static render() {
         get(Settings.baseURL+'/home').then(function(response) {
-            let a = JSON.parse(response);
-            a.forEach(element => {
+            let home = JSON.parse(response);
+            document.getElementById("page").innerHTML ="";
+            home.forEach(element => {
               document.getElementById("page").innerHTML = template(element);
             });   
+          }).catch(function(error) {
+            console.log("Failed!", error);
+          })
+        
+          get(Settings.baseURL+'/tarifa').then(function(response) {
+            let a = JSON.parse(response);
+            let tarifas = "";
+            a.results.forEach(element => {
+              console.log(element);
+              tarifas += tarifa(element);
+            });
+            let result = `
+            <div class="flex-container-tarifas"> 
+              ${tarifas} 
+            </div>
+            `;
+            document.getElementById("tarifas").innerHTML = result;
           }).catch(function(error) {
             console.log("Failed!", error);
           })
