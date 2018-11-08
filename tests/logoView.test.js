ad7b4a2f7eaad8f9eba0cd12d316e4eac5cb6042
Rallkus <serhuegi@gmail.com>
2018-11-08 19:53:51 +0100

    Now logoView.test.js tests if data is not provided

--- tests/logoView.test.js
+++ tests/logoView.test.js
@@ -3,3 +3,7 @@ import {template} from '../src/modules/logo/view/logoView';
 test('Sets the logo', () => {
     expect(template("dsa")).toMatch(/dsa/);
   });
+
+test('No data provided', () => {
+  expect(template()).toMatch(/src="undefined"/);
+});
