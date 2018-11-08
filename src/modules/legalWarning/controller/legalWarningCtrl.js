395d404d547403b81d997f053e632443f55120aa
Rallkus <serhuegi@gmail.com>
2018-11-08 09:13:06 +0100

    Now legalWarningCtrl.js doesn't need a template

--- src/modules/legalWarning/controller/legalWarningCtrl.js
+++ src/modules/legalWarning/controller/legalWarningCtrl.js
@@ -1,6 +1,5 @@
 import {get} from '../../../utils/utils';
 import {Settings} from '../../../settings';
-import {template} from '../view/legalWarning.html';
 /**This is the legal warning controller */
 class legalWarningController {
 
@@ -10,18 +9,13 @@ class legalWarningController {
   
     /** render  */
     static render() {
-    document.getElementById("page").innerHTML = template();
     get(Settings.baseURL+'/datos_empresa').then(function(response) {
       let datos_empresa = JSON.parse(response);
 
       let a=datos_empresa.textos.filter(datos => datos.key.match(/jumbotron_legal/));
       console.log(a);
-      document.getElementById("datos_generales").innerHTML=a.filter(datos => datos.key.match(/datos_generales/))[0].content;
-      document.getElementById("politica_privacidad").innerHTML=a.filter(datos => datos.key.match(/politica_privacidad/))[0].content;
-      document.getElementById("condiciones_uso").innerHTML=a.filter(datos => datos.key.match(/condiciones_uso/))[0].content;
-      document.getElementById("responsabilidades").innerHTML=a.filter(datos => datos.key.match(/responsabilidades/))[0].content;
-      document.getElementById("propiedad_intelectual").innerHTML=a.filter(datos => datos.key.match(/propiedad_intelectual/))[0].content;
-      document.getElementById("ley_aplicable").innerHTML=a.filter(datos => datos.key.match(/ley_aplicable/))[0].content;
+      document.getElementById("page").innerHTML=a[0].content;
+      //document.getElementById("ley_aplicable").innerHTML=a.filter(datos => datos.key.match(/ley_aplicable/))[0].content;
       //console.log(datos_empresa.textos);
     }).catch(function(error) {
       console.log("Failed!", error);
