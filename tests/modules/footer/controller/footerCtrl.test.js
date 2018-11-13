74b8c6e7b3cf03ccd05a94799aa8555987bd3d58
Rallkus <serhuegi@gmail.com>
2018-11-13 12:19:35 +0100

    Updated footerCtrl.test.js and headerCtrl.test.js

--- tests/modules/footer/controller/footerCtrl.test.js
+++ tests/modules/footer/controller/footerCtrl.test.js
@@ -1,11 +1,37 @@
-import FooterController from '../../../../src/modules/footer/controller/footerCtrl';
+import footerCtrl from '../../../../src/modules/footer/controller/footerCtrl';
 
-jest.mock('../../../../src/modules/footer/controller/footerCtrl');
+const rrss = {facebook:"paco", twitter:"perolo"};
+const $ = require('jquery');
 beforeAll(()=>{
-    FooterController.mockClear();
+      // Set up our document body
+  document.body.innerHTML =`<!DOCTYPE html>
+  <html lang="es">
+    <head>
+      <meta http-equiv="content-type" content="text/html; charset=utf-8">
+      <title>title</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <script type="text/javascript" src="main.js"></script>
+    
+      <script> </script>
+    </head>
+    <body>
+      <header id="menuShow">
+      </header>
+      <section>
+          <div class="flex-container">
+            <div class="firstElementFlex" id="logo"></div>
+            <div id="page"></div>            
+          </div>     
+      </section>
+      <footer id="footer">
+      </footer>
+    </body>
+    
+  </html>`;
 })
 
-test('We check if FooterController is called', () => {
-    FooterController.render();
-    expect(FooterController.render).toHaveBeenCalledTimes(1);
+test('We check if HeaderController is called', () => {
+    footerCtrl.render(rrss);
+    expect($('#footer').children.length).toBeGreaterThan(1);    
 });
--- tests/modules/header/controller/headerCtrl.test.js
+++ tests/modules/header/controller/headerCtrl.test.js
@@ -1,11 +1,35 @@
-import HeaderController from '../../../../src/modules/header/controller/headerCtrl';
-
-jest.mock('../../../../src/modules/header/controller/headerCtrl');
+import headerCtrl from '../../../../src/modules/header/controller/headerCtrl';
+const $ = require('jquery');
 beforeAll(()=>{
-    HeaderController.mockClear();
+      // Set up our document body
+  document.body.innerHTML =`<!DOCTYPE html>
+  <html lang="es">
+    <head>
+      <meta http-equiv="content-type" content="text/html; charset=utf-8">
+      <title>title</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <script type="text/javascript" src="main.js"></script>
+    
+      <script> </script>
+    </head>
+    <body>
+      <header id="menuShow">
+      </header>
+      <section>
+          <div class="flex-container">
+            <div class="firstElementFlex" id="logo"></div>
+            <div id="page"></div>            
+          </div>     
+      </section>
+      <footer id="footer">
+      </footer>
+    </body>
+    
+  </html>`;
 })
 
 test('We check if HeaderController is called', () => {
-    HeaderController.render();
-    expect(HeaderController.render).toHaveBeenCalledTimes(1);
+    headerCtrl.render();
+    expect($('#menuShow').children.length).toBeGreaterThan(1);    
 });
