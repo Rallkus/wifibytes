bd1e155c97c5b543584b67f80c1ada584440953f
Rallkus <serhuegi@gmail.com>
2018-11-13 12:22:20 +0100

    logoCtrl.test.js added

---
+++ tests/modules/logo/controller/logoCtrl.test.js
@@ -0,0 +1,37 @@
+import logoCtrl from '../../../../src/modules/logo/controller/logoCtrl';
+
+const logo="logo";
+const $ = require('jquery');
+beforeAll(()=>{
+      // Set up our document body
+  document.body.innerHTML =`<!DOCTYPE html>
+  <html lang="es">
+    <head>
+      <meta http-equiv="content-type" content="text/html; charset=utf-8">
+      <title>title</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <script type="text/javascript" src="main.js"></script>
+    
+      <script> </script>
+    </head>
+    <body>
+      <header id="menuShow">
+      </header>
+      <section>
+          <div class="flex-container">
+            <div class="firstElementFlex" id="logo"></div>
+            <div id="page"></div>            
+          </div>     
+      </section>
+      <footer id="footer">
+      </footer>
+    </body>
+    
+  </html>`;
+})
+
+test('We check if HeaderController is called', () => {
+    logoCtrl.render(logo);
+    expect($('#logo').children.length).toBeGreaterThan(1);    
+});
